9eab32fc4338ec9bb605c1b117397270efa2dabd
Haris87 <mpouxlhs_xar@yahoo.com>
2018-01-09 14:49:35 +0200

    fixed params bug

--- exchange.js
+++ exchange.js
@@ -65,7 +65,7 @@ function getCandlesticks(tradePair, interval, callback) {
 
     var ticks = ticksArray.map(function(tick) {
       return {
-        ticker: params.tradePair,
+        ticker: tradePair,
         open: Number(tick[1]),
         high: Number(tick[2]),
         low: Number(tick[3]),
@@ -107,79 +107,6 @@ module.exports = {
 	getCandlesticksRealTime: getCandlesticksRealTime,
 	getCandlesticks: getCandlesticks
 };
-
-
-function trade(ticks, multiplier) {
-  var margin, movingAvg, price, color;
-  for (var i = 1; i < ticks.length; i++) {
-    price = ticks[i].close;
-    var time = ticks[i].date;
-    movingAvg = movingAverage(ticks.slice(0, i));
-    margin = movingAvg * multiplier;
-
-
-    color = getTrendColor(price, movingAvg, margin);
-    // console.log("Margin\t", color, movingAvg, margin, [movingAvg-margin, movingAvg+margin]);
-    //console.debug(color, ticks[i].date+"\tPRICE: "+price+"\tAVG:"+movingAvg);
-
-    // decide trade
-    if (previousColor == 'R' && (color == 'Y' || color == 'G') && previousTrade == 'SELL') {
-      //buy
-      buy(price, time);
-      lastBuy = price;
-    }
-
-    if (previousColor == 'G' && (color == 'Y' || color == 'R') && previousTrade == 'BUY') {
-      //sell
-      var isSellProfitable = price + price * params.tradingFee - lastBuy > 0;
-      if (isSellProfitable) {
-        sell(price, time);
-        checkProfitability = false;
-      } else {
-        // check next candlesticks for sell profitability
-        checkProfitability = true;
-      }
-    }
-
-    // check next candlesticks for sell profitability
-    if (checkProfitability) {
-      var isSellProfitable = price + price * params.tradingFee - lastBuy > 0;
-      if (isSellProfitable) {
-        console.log('FOUND PROFITABLE SELL!');
-        sell(price, time);
-        checkProfitability = false;
-      }
-    }
-
-    previousColor = color;
-
-    // for testing only, delete in live
-    // if(i == ticks.length-1 && previousTrade == 'BUY'){
-    //   console.log('LAST SELL');
-    //   sell(price, time);
-    // }
-
-  }
-
-
-  //console.log('EARNED\t', wallet);
-
-	console.debug = function(color, text) {
-	  if (color == 'R') {
-	    console.log("\x1b[31m%s\x1b[0m", text);
-	  }
-
-	  if (color == 'G') {
-	    console.log("\x1b[32m%s\x1b[0m", text);
-	  }
-
-	  if (color == 'Y') {
-	    console.log("\x1b[33m%s\x1b[0m", text);
-	  }
-
-	}
-
-}
 
 // Get bid/ask prices
 //binance.allBookTickers(function(json) {
